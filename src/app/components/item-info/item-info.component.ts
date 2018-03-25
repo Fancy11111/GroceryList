8f35be9408703ec21953e648a16d71a2dfafb45c
daniel.fenz.at@gmail.com <dani.fenz@aon.at>
2018-03-25 12:27:56 +0200

    added feature: priceSum (not fully functional)

--- src/app/components/item-info/item-info.component.ts
+++ src/app/components/item-info/item-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../../services/message.service';
+import { ItemListService } from '../../services/item-list.service';
 
 declare var M: any; // Silence Typescript warning
 @Component({
@@ -8,11 +9,34 @@ declare var M: any; // Silence Typescript warning
   styleUrls: ['./item-info.component.css']
 })
 export class ItemInfoComponent implements OnInit {
+  public priceSum = 0;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private itemListService: ItemListService) { }
 
   ngOnInit() {
     this.messageService.getMessagesSubject().subscribe(msg => M.toast({ html: msg, classes: 'message' }) );
+    this.subscribeItemChanges();
+    this.evalPriceSum();
+  }
+
+  public subscribeItemChanges() {
+    this.itemListService.getItemsChangedEmitter().subscribe(() => {
+      this.priceSum = 0;
+      this.evalPriceSum();
+    });
+  }
+
+  public evalPriceSum() {
+    this.itemListService.items.forEach((item, index, items) => {
+      if (item.erledigt !== 1) {
+        // I need to multiply and divide to remove the floating point inaccuracy
+        this.priceSum = (this.priceSum * 10 + item.kosten * 10) / 10;
+      }
+    });
+  }
+
+  public evalStatus() {
+
   }
 
 }
