4c47dcae6e3d0d37a1e87ee7785e151f1c68c579
Daniel Fenz <dani.fenz@aon.at>
2018-05-05 12:17:23 +0200

    updated the express rest api

--- grocery-api/app.js
+++ grocery-api/app.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 let app = express();
 
-const listSchema = require('./models/einkaufsliste');
+const listSchema = require('./models/itemlist');
+//const userSchema;
+
 const itemGet = require('./routes/itemGet');
 const itemPost = require('./routes/itemPost');
 const itemPut = require('./routes/itemPut');
@@ -12,36 +14,44 @@ const itemDelete = require('./routes/deletePost');
 
 let dbPort = process.env.dbPort || 27017;
 let listModel;
+function conn_database(port = 27017, url = 'localhost') {
+    console.log(`Connecting to mongodb on: ${url}/${port}`);
+    mongoose.connect(`mongodb://${url}:${port}/einkaufsliste`);
+    let db = mongoose.connection;
+    db.on('error', () => {
+        console.error.bind(console, 'connection error:');
+        console.log('Program will exit, please check database connectivity')
+        process.exit(-1)
+    });
+    db.once('open', function () {
+        console.log('successful db connection');
+        listModel = db.model('einkaufsliste', listSchema);
+        app.locals.listModel = listModel;
+    });
+}
 
-mongoose.connect('mongodb://localhost:' + dbPort + '/einkaufsliste');
-let db = mongoose.connection;
-db.on('error', () => {
-    console.error.bind(console, 'connection error:'); 
-    console.log('Program will exit, please check database connectivity')
-    process.exit(-1)
-});
-db.once('open', function () {
-    console.log('successful db connection');
-    listModel = db.model('einkaufsliste', listSchema);
-    app.locals.listModel = listModel;
-});
+function start_express_server(port){
+    // Middleware
+    app.use(bodyParser.json())
+    app.use(cors());
+
+    // Route /einkaufsliste
+    app.route('/einkaufsliste')
+        .get(itemGet.getAll)
+        .post(itemPost);
+
+    // Route /einkaufsliste/:id
+    app.route('/einkaufsliste/:id')
+        .get(itemGet.getOne)
+        .put(itemPut)
+        .delete(itemDelete);
+
+    // TODO: User auth routes
+
+    app.listen(port, () => console.log(`running on localhost:${port}`));
+}
+
+conn_database();
 
 
-let port = process.env.port || 5000;
-
-// Middleware
-app.use(bodyParser.json())
-app.use(cors());
-
-// Route /einkaufsliste
-app.route('/einkaufsliste')
-    .get(itemGet.getAll)
-    .post(itemPost);
-
-// Route /einkaufsliste/:id
-app.route('/einkaufsliste/:id')
-    .get(itemGet.getOne)
-    .put(itemPut)
-    .delete(itemDelete);
-
-app.listen(port, () => console.log('running on ' + port));
+start_express_server(process.env.port || 5000);
